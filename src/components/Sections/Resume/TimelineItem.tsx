8e38dca80db47c664768d11809c70b07d813b0fe
MuhammadHakeem <muhdhakeem526@gmail.com>
2024-01-08 23:49:03 +0800

    fix: resume section mobile responsive

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -8,15 +8,15 @@ const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
     <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
       <div className="flex flex-col pb-4">
         <div className='flex flex-row justify-between flex-nowrap'>
-          <h2 className="text-xl font-bold w-8/12">{title}</h2>
+          <h2 className="text-xl font-bold w-8/12 flex justify-start text-start">{title}</h2>
           <div className='w-1/6 h-auto'>
             <img src={logo}/>
           </div>
         </div>
-        <div className="flex items-center justify-center gap-x-2 md:justify-start">
-          <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
+        <div className="flex flex-row items-center justify-start gap-x-2 sm:flex-row">
+          <span className=" text-sm font-medium italic">{location}</span>
           <span>•</span>
-          <span className="flex-1 text-sm sm:flex-none">{date}</span>
+          <span className="text-sm sm:flex-none">{date}</span>
         </div>
       </div>
       {content}
