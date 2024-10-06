40acf7e993e18d0dd7ad40287fbf0e09b4b4d1cd
MuhammadHakeem <muhdhakeem526@gmail.com>
2024-10-06 22:49:06 +0800

    fix: lint error

--- src/components/Sections/TechStack/TechStack.tsx
+++ src/components/Sections/TechStack/TechStack.tsx
@@ -8,7 +8,7 @@ import Section from '../../Layout/Section';
 // import Socials from '../Socials';
 
 const Hero: FC = memo(() => {
-  const {imageSrc, name,} = heroData;
+  const { imageSrc, name } = heroData;
 
   return (
     <Section noPadding sectionId={SectionId.TechStack} className='py-8 px-8'>
@@ -24,71 +24,27 @@ const Hero: FC = memo(() => {
           <h2 className='uppercase text-center sporty-green'>My Technological Stack & Frameworks:</h2>
         </div>
         <div className="flex flex-col flex-wrap justify-center items-center content-center sm:flex-row py-6 min-h-[100vh] sm:items-baseline p-6">
-          <div className='pt-4 sm:ml-4 !border-0 w-fit'>
-            <div className="flex flex-row justify-center bg-white rounded-lg p-4 relative">
-              {jsFrameworks.map((item) => (
-                <div className="text-black flex flex-col p-2 flex items-center">
-                  <item.icon className="h-14 w-14 sm:h-14 sm:w-14"/>
-                  <label className='font-bold self-center text-gray-600'>{item.name}</label>
-                </div>
-              ))}
-              <div className='absolute left-0 bottom-0 h-2 bg-red-600 h-4 w-full rounded-lg'></div>
+          {[
+            { items: jsFrameworks, color: 'red-600', label: 'javascript frameworks' },
+            { items: mobileFrameWorks, color: 'blue-600', label: 'mobile frameworks' },
+            { items: progLang, color: 'blue-600', label: 'programming language' },
+            { items: cssFrameWorks, color: 'green-600', label: 'CSS frameworks' },
+            { items: otherSoftware, color: 'green-600', label: 'other software' }
+          ].map(({ items, color, label }, index) => (
+            <div className='pt-4 sm:ml-4 !border-0 w-fit' key={index}>
+              <div className="flex flex-row justify-center bg-white rounded-lg p-4 relative">
+                {items.map((item) => (
+                  <div className="text-black flex flex-col p-2 flex items-center" key={item.name}>
+                    <item.icon className="h-14 w-14 sm:h-14 sm:w-14" />
+                    <label className='font-bold self-center text-gray-600'>{item.name}</label>
+                  </div>
+                ))}
+                <div className={`absolute left-0 bottom-0 h-2 ${color} h-4 w-full rounded-lg`}></div>
+              </div>
+              <h1 className='pt-2 sm:pt-4 font-bold capitalize text-center text-white bottom-0'>{label}</h1>
             </div>
-            <h1 className='pt-2 sm:pt-4 font-bold capitalize text-center text-white bottom-0'>javascript frameworks</h1>
-          </div>
-
-          <div className='pt-4 sm:ml-4 !border-0 w-fit'>
-            <div className="flex flex-row justify-center bg-white rounded-lg p-4 relative">
-              {mobileFrameWorks.map((item) => (
-                <div className="text-black flex flex-col p-2 flex items-center">
-                  <item.icon className="h-14 w-14 sm:h-14 sm:w-14"/>
-                  <label className='font-bold self-center text-gray-600'>{item.name}</label>
-                </div>
-              ))}
-              <div className='absolute left-0 bottom-0 h-2 bg-blue-600 h-4 w-full rounded-lg'></div>
-            </div>
-            <h1 className='pt-2 sm:pt-4 font-bold capitalize text-center text-white bottom-0'>mobile frameworks</h1>
-          </div>
-          
-          <div className='pt-4 sm:ml-4 !border-0 w-fit'>
-            <div className="flex flex-row justify-center bg-white rounded-lg p-4 relative">
-              {progLang.map((item) => (
-                <div className="text-black flex flex-col p-2 flex items-center">
-                  <item.icon className="h-14 w-14 sm:h-14 sm:w-14"/>
-                  <label className='font-bold self-center text-gray-600'>{item.name}</label>
-                </div>
-              ))}
-              <div className='absolute left-0 bottom-0 h-2 bg-blue-600 h-4 w-full rounded-lg'></div>
-            </div>
-            <h1 className='pt-2 sm:pt-4 font-bold capitalize text-center text-white bottom-0'>programming language</h1>
-          </div>
-
-          <div className='pt-4 sm:ml-4 !border-0 w-fit'>
-            <div className="flex flex-row justify-center bg-white rounded-lg p-4 relative">
-              {cssFrameWorks.map((item) => (
-                <div className="text-black flex flex-col p-2 flex items-center">
-                  <item.icon className="h-14 w-14 sm:h-14 sm:w-14"/>
-                  <label className='font-bold self-center text-gray-600'>{item.name}</label>
-                </div>
-              ))}
-              <div className='absolute left-0 bottom-0 h-2 bg-green-600 h-4 w-full rounded-lg'></div>
-            </div>
-            <h1 className='pt-2 sm:pt-4 font-bold capitalize text-center text-white bottom-0'>CSS frameworks</h1>
-          </div>
-          <div className='pt-4 sm:ml-4 !border-0 w-fit'>
-            <div className="flex flex-row flex-wrap justify-center bg-white rounded-lg p-4 relative">
-              {otherSoftware.map((item) => (
-                <div className="text-black flex flex-col p-2 flex items-center">
-                  <item.icon className="h-14 w-14 sm:h-14 sm:w-14"/>
-                  <label className='font-bold self-center text-gray-600'>{item.name}</label>
-                </div>
-              ))}
-              <div className='absolute left-0 bottom-0 h-2 bg-green-600 h-4 w-full rounded-lg'></div>
-            </div>
-            <h1 className='pt-2 sm:pt-4 font-bold capitalize text-center text-white bottom-0'>other software</h1>
-          </div>
+          ))}
         </div>
-        
       </div>
     </Section>
   );
