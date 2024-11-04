1bc0b997e8fe8e7375cdf05cf46d4cb67e5224de
MuhammadHakeem <muhdhakeem526@gmail.com>
2024-11-05 00:22:02 +0800

    feat: feature update

--- src/components/Sections/TechStack/TechStack.tsx
+++ src/components/Sections/TechStack/TechStack.tsx
@@ -16,11 +16,11 @@ const Hero: FC = memo(() => {
   const { imageSrc, name } = heroData;
 
   const techStacks = [
-    { items: jsFrameworks, color: 'red-600', label: 'JavaScript Frameworks ' },
+    { items: jsFrameworks, color: 'red-600', label: 'JavaScript Frameworks' },
     { items: mobileFrameWorks, color: 'blue-600', label: 'Mobile Frameworks' },
     { items: progLang, color: 'yellow-600', label: 'Programming Languages' },
     { items: cssFrameWorks, color: 'green-600', label: 'CSS Frameworks' },
-    { items: otherSoftware, color: 'purple-600', label: 'Other Software ' },
+    { items: otherSoftware, color: 'purple-600', label: 'Other Software' },
   ];
 
   return (
